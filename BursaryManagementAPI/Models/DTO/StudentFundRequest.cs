b56eb000a0bd9001d86457fe1d27a3d91d2e87e6
Philani Ngcamu <Philaningcamu18@gmail.com>
2024-02-17 23:03:48 +0200

    merge conflicts

---
+++ BursaryManagementAPI/Models/DTO/StudentFundRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BursaryManagementAPI.Models.DTO
+{
+    public class StudentFundRequest
+    {
+        
+        public int ID { get; set; }
+
+        
+        public int StudentID { get; set; }
+        public int CreatedDate { get; set; }
+        public int ModifiedDate { get; set; }
+        public int Grade { get; set; }
+        public decimal Amount { get; set; }
+        public int ApplicationStatus { get; set; }
+        public string? Comment { get; set; }
+
+        // Optional: Additional properties or methods can be added as needed
+    }
+
+}
